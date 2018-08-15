35f5b4d13121cf38db5479729996f88a8de6ba10
Lil_Red <thomasplux@gmail.com>
2018-08-15 11:24:13 -0400

    made a rough outline for MatchBanner

--- components/MatchBanner.js
+++ components/MatchBanner.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
-import { Button } from 'react-native-elements';
 import style from '../public/style';
+import firestore from '../firestore';
 
+export default class LotBanner extends React.Component {
 
+  // props is just the lot information from firestore
+  // so it has driverId, dropoffLocation, offer, passengerId, pickupLocation, pickupTime, screenshot
+  state = { ...this.props.matchData, 
+    driverName: '',
+    driverPhone: ''
+    // We will need to get stuff from the driving credentials field
+  };
 
-const MatchBanner = ({ navigation, name, phoneNumber, dropoffLocation, pickupTime }) => (
-  <View style={style.container}>
-    <Text style={style.title}>BayRide</Text>
-    <View style={style.containerRow}>
-     <Text style={style.title}>Name {name}</Text>
-     <Text style={style.title}>Pickup Time: {pickupTime}</Text>
+  componentDidMount () {
+    firestore.collection("users").doc(this.state.driverId).get().then(doc => {
+      if (doc.exists) {
+        driverName = doc.data().name;
+        driverPhone = doc.data().phone;
+      }
+    })
+  }
 
-      <Text style={style.title}>Phone number: {phoneNumber}</Text>
-      <Text style={style.title}>Drop Off location: {dropoffLocation}</Text>
+  render () {
+    return (
+      <View>
+        <Text>Your requested trip</Text>
+        {this.state.driverId 
+          ? <View>
+              <Text>You are currently matched with:</Text>       
+              <Text>{this.state.driverName}</Text>
+              <Text>Phone Number: {this.state.driverPhone}</Text>
+              <Text>They are offering ${this.state.offer} for this trip</Text>
+              <Text>Here is where the car information etc. can go</Text>
+            </View> 
+          : <View>
+            <Text>You aren't currently matched with anyone</Text>
+            <Text>You're asking for ${this.state.offer} for this trip</Text>
+            </View>
+        }
+        <Text>Time until pickup: {this.state.pickupTime}</Text>
+        <Text>pickup location: {this.state.dropoffLocation}</Text>
+      </View>
+    );
+  }
 
-    </View>
-  </View>
-);
-
-export default MatchBanner;
+}

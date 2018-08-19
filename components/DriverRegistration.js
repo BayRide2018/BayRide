b6d5c3645e45759868af1958b78f5f57fd5558b9
Lil_Red <thomasplux@gmail.com>
2018-08-19 02:06:42 -0400

    made very basic and unfinished version of DriverRegistration. Also updated the signup form to reflect the temporary way of seeing whether or not someone is allowed to drive: the canDrive property

--- components/DriverRegistration.js
+++ components/DriverRegistration.js
@@ -1,1 +1,50 @@
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, View, Button, Text, Platform } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+import { signup } from '../fireMethods';
+import firestore from '../firestore';
+import firebase from 'firebase';
+import style from '../public/style';
+import { Picker, DatePicker } from 'react-native-wheel-pick';
+import ViewPhotos from './ViewPhotos';
 
+
+export default class DriverRegistration extends Component {
+
+	handleSubmit = async () => {
+
+    const userEmail = await firebase.auth().currentUser.email;
+    let userId;
+    await firestore.collection("users").where("email", "==", userEmail).get().then(users => {
+      users.forEach(user => {
+        userId = user.id;
+      })
+    })
+    await firestore.collection("users").doc(userId).update({
+      drivingInformation: { canDrive: true }
+    })
+    this.props.navigation.navigate('DriverHome');
+  }
+
+
+  handleBack = async () => {
+    this.props.navigation.navigate('PassengerHome');
+  }
+
+  render() {
+    return (
+      <View style={{ alignItems: 'center', flex: 1 }}>
+        <Text>This is the sample driver registration page</Text>
+        <Text>For now, just click the button, and you'll be registered as a driver!</Text>
+        <Text>Later, registration will be a more formal process, with real requirements</Text>
+        <View style={style.button}>
+          <Button
+            title="Sign up to Drive"
+            onPress={this.handleSubmit}
+          />
+          <Button style={style.backButton} title='Go Back' onPress={this.handleBack} />
+        </View>
+      </View>
+    );
+  }
+}

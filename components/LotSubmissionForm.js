256b66181c45d27a662342ed5b1fd01fb67febe4
Jeton Lolovic <jetlol79@gmail.com>
2018-08-22 17:14:53 -0400

    google places autocomplete is done

--- components/LotSubmissionForm.js
+++ components/LotSubmissionForm.js
@@ -8,6 +8,7 @@ import firebase from 'firebase';
 import style from '../public/style';
 import { Picker, DatePicker } from 'react-native-wheel-pick';
 import ViewPhotos from './ViewPhotos';
+import GooglePlacesInput from './GooglePlacesInput';
 
 export default class LotSubmissionForm extends Component {
 
@@ -23,17 +24,17 @@ export default class LotSubmissionForm extends Component {
 		driverId: '',
 		showPicker: false,
 		pickupTime: 'Pick up time in'
-  }
+	}
 
-  async componentDidMount() {
+	async componentDidMount() {
 
 			const passengerEmail = await firebase.auth().currentUser.email;
 			await store.collection('users').where('email', '==', passengerEmail).get().then(users => {
 				users.forEach(user => {
 					this.setState({passengerId: user.id});
-        });
-      });
-  }
+				});
+			});
+	}
 
 	handleSubmit = async () => {
 		const screenshot = this.state.screenshot;
@@ -43,17 +44,17 @@ export default class LotSubmissionForm extends Component {
 		const offer = this.state.offer;
 		const passengerId = this.state.passengerId;
 
-    store.collection("lots").add({
-      screenshot,
-      pickupTime,
-      pickupLocation,
-      dropoffLocation,
-      offer,
-      passengerId,
-      driverId: null
-    });
+		store.collection("lots").add({
+			screenshot,
+			pickupTime,
+			pickupLocation,
+			dropoffLocation,
+			offer,
+			passengerId,
+			driverId: null
+		});
 	}
-	
+
 	handleUseCurrentLocation = async () => {
 		/**
 		 * location has this form:
@@ -106,10 +107,7 @@ export default class LotSubmissionForm extends Component {
 						onChangeText={pickupTime => this.setState({ pickupTime })}
 					/>
 					<FormLabel>Pickup Location</FormLabel>
-					<FormInput
-					placeholder="Please enter pickup location"
-						onChangeText={pickupLocation => this.setState({ pickupLocation })}
-					/>
+					<GooglePlacesInput />
 					<Button title="Use my current location for pick up" onPress={this.handleUseCurrentLocation} />
 					<FormLabel>Drop off Location</FormLabel>
 					<FormInput
@@ -132,5 +130,5 @@ export default class LotSubmissionForm extends Component {
 				</View>
 			</View>
 		);
-  }
+	}
 }

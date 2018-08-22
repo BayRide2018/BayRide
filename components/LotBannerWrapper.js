83a958d94d373a72f078d6c026623d2eb4103294
Lil_Red <thomasplux@gmail.com>
2018-08-22 04:50:49 -0400

    Timer is working pretty swell

--- components/LotBannerWrapper.js
+++ components/LotBannerWrapper.js
@@ -7,21 +7,32 @@ import TimerCountdown from 'react-native-timer-countdown';
  
 export default class LotBannerWrapper extends React.Component {
 
+  state = {
+    showThisBanner: true
+  }
+
   handleFinish = () => {
-    // Very Important
+    // Very Important Whatever happens here...
+    // Send a notification to the Passenger (they can adjust the bid [add time or lower price] or just have it deleted)
+    // and hide this for now
+    this.setState({ showThisBanner: false })
   }
 
   render () {
     return (
       <View>
-        <TimerCountdown
-            initialSecondsRemaining={1000*60}
-            onTick={secondsRemaining => console.log('tick', secondsRemaining)}
-            onTimeElapsed={this.handleFinish}
-            allowFontScaling={true}
-            style={{ fontSize: 20 }}
-        />
-        <LotBanner lotData={this.props.lotData} />
+        {this.state.showThisBanner 
+        ? <View>
+            <TimerCountdown
+              initialSecondsRemaining={1000*60}
+              onTick={secondsRemaining => console.log('tick', secondsRemaining)}
+              onTimeElapsed={this.handleFinish}
+              allowFontScaling={true}
+              style={{ fontSize: 20 }}
+            />
+            <LotBanner lotData={this.props.lotData} /> 
+          </View>
+        : null}
       </View>
     )
   }

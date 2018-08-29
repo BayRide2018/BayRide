27d374e790e77e4dd70ae6be228d49082080a4ed
Lil_Red <thomasplux@gmail.com>
2018-08-29 11:42:17 -0400

    updated DriverHome to pass the lot ids to LotBannerWrapper for use with expireLots

--- components/LotBannerWrapper.js
+++ components/LotBannerWrapper.js
@@ -4,12 +4,13 @@ import LotBanner from "./LotBanner";
 import style from '../public/style';
 import TimerCountdown from 'react-native-timer-countdown';
 import { store } from '../fire';
+import { expireLot } from '../fireMethods';
 
 
 export default class LotBannerWrapper extends React.Component {
 
 	state = {
-	showThisBanner: true
+		showThisBanner: true
 	}
 
 	handleFinish = () => {
@@ -18,19 +19,20 @@ export default class LotBannerWrapper extends React.Component {
 	// and hide this for now
 		this.setState({ showThisBanner: false });
 
-		// Here is where we should call expireLot({lotId});
+		// Here is where we should call expireLot(this.props.lotData.lotId);
 		// It should replace the below call
 
-		store.collection('lots').where('passengerId', '==', this.props.lotData.passengerId).get()
-			.then((lots) => {
-				lots.forEach(lot => {
-					if (lot.data().driverId) {
-						console.log('i found the driver id', lot.data().driverId);
-					} else {
-						lot.ref.delete();
-					}
-				});
-			});
+		expireLot(this.props.lotData.lotId);
+		// store.collection('lots').where('passengerId', '==', this.props.lotData.passengerId).get()
+		// 	.then((lots) => {
+		// 		lots.forEach(lot => {
+		// 			if (lot.data().driverId) {
+		// 				console.log('i found the driver id', lot.data().driverId);
+		// 			} else {
+		// 				lot.ref.delete();
+		// 			}
+		// 		});
+		// 	});
 	}
 
 	render () {

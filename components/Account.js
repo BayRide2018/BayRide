559e6ced2db3ad700c0461fb19ac94e6538c2f0f
Jeton Lolovic <jetlol79@gmail.com>
2018-08-29 11:58:57 -0400

    a user can now edit their account info

--- components/Account.js
+++ components/Account.js
@@ -9,51 +9,43 @@ import firebase from 'firebase';
 export default class Account extends Component {
 
 	state = {
-        name: '',
+		name: '',
 		email: '',
 		password: '',
-        phone: '',
-        defaultSetting: '',
-        paymentInformation: {}, // I think that these two will be editable somewhere else.. There will be other forms that it 
-        drivingInformation: {}, // Takes them to.. The other tabs in the drawer
-        id: '' // This is here for when we want to add an edit button later
-    };
-    
-    componentDidMount () {
-        store.collection("users").where("email", "==", auth.currentUser.email).get()
-        .then(users => {
-            users.forEach(user => {
-                this.setState({name: user.data().name, email: user.data().email, password: user.data().password, phone: user.data().phone, defaultSetting: user.data().defaultSetting , paymentInformation: user.data().paymentInformation, drivingInformation: user.data().drivingInformation, id: user.id})
-            })
-        })
-    }
+		phone: '',
+		defaultSetting: '',
+		paymentInformation: {}, // I think that these two will be editable somewhere else.. There will be other forms that it
+		drivingInformation: {}, // Takes them to.. The other tabs in the drawer
+		id: '' // This is here for when we want to add an edit button later
+	};
 
-	handleSubmit = async () => {
-        console.log("This is where the edits should happen. (Nothing happens yet).");
-        console.log("Please note: We can't just let users edit their emails... Important to talk about this");
+	componentDidMount () {
+		store.collection("users").where("email", "==", auth.currentUser.email).get()
+		.then(users => {
+			users.forEach(user => {
+				this.setState({name: user.data().name, email: user.data().email, password: user.data().password, phone: user.data().phone, defaultSetting: user.data().defaultSetting , paymentInformation: user.data().paymentInformation, drivingInformation: user.data().drivingInformation, id: user.id})
+			})
+		})
 	}
 
+	handleSubmit = async () => {
+		store.collection('users').doc(auth.currentUser.email).update({name: this.state.name,   phone: this.state.phone});
+		// We probably want to add more fields for the user to edit but for now its fine
+		this.props.navigation.navigate('MainScreen');
+	}
+
+
 	render() {
-        const { name, email, password, phone, defaultSetting, paymentInformation, drivingInformation, id } = this.state;
+		const { name, email, password, phone, defaultSetting, paymentInformation, drivingInformation, id } = this.state;
 		return (
 			<View>
-                <Text>This is your profile!!</Text>
-                <Text>Please note: We can't just let users edit their emails... Important to talk about this</Text>
-                <Text>Name: </Text>
-                <Text>{name}</Text>
-                <Text>email: </Text>
-                <Text>{email}</Text>
-                <Text>password: </Text> /** We'll want something nice here, like have it not show the password. Also editing some of this stuff will be tricky */
-                <Text>{password}</Text>
-                <Text>Phone number: </Text>
-                <Text>{phone}</Text>
-                <Text>defaultSetting: </Text>
-                <Text>{defaultSetting}</Text>
-                <Text>id: </Text>
-                <Text>{id}</Text>
-                <Text>paymentInformation: </Text>
-                <Text>drivingInformation: (these are empty on purpose for now)</Text>
-                <Button title="Edit" onPress={this.handleSubmit} />
+				<Text>This is your profile!!</Text>
+
+				<FormLabel>Name: </FormLabel>
+				<FormInput placeholder={name} onChangeText={name => this.setState({name: name})}></FormInput>
+				<FormLabel>Phone number: </FormLabel>
+				<FormInput placeholder={phone} onChangeText={phone => this.setState({phone: phone})}></FormInput>
+				<Button title="Save Changes" onPress={this.handleSubmit} />
 			</View>
 		);
 	}

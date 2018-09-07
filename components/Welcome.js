a20a3d338d8073481169f7d0932bf37dfca2a3bd
Lil_Red <thomasplux@gmail.com>
2018-09-07 14:45:59 -0400

    added logo to welcome page

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { Button } from 'react-native-elements';
 import style from '../public/style';
 
 const Welcome = ({ navigation }) => (
 	<View style={style.container}>
-		<Text style={style.title}>BayRide</Text>
+			<Image source={require('../public/logo1.png')} style={{ marginTop: 150, height: 230, width: 230 }} />
 		<View style={style.containerRow}>
 			<Button style={style.welcomeButton} title="Sign Up" onPress={() => navigation.navigate('Signup')} />
 			<Button style={style.welcomeButton} title="Login" onPress={() => navigation.navigate('Login')} />

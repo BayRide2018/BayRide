bb7c1136ebfe765ccafa7a049f18e75726d487f2
Lil_Red <thomasplux@gmail.com>
2018-09-10 01:01:18 -0400

    reworking winner

--- components/Winner.js
+++ components/Winner.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Text, View, TouchableOpacity, Button } from 'react-native';
 import style from '../public/style';
 import { store, auth } from '../fire';
-import Modal from "react-native-modal";
 import getDirections from 'react-native-google-maps-directions';
 import call from 'react-native-phone-call';
 
@@ -10,20 +9,21 @@ import call from 'react-native-phone-call';
 export default class Winner extends React.Component {
 
 	state = {
-		isModalVisible: true,
-		// lot : {}
+		lot : {},
 		passenger : {}
 	};
 
 	componentDidMount = async () => {
-		await store.collection("users").doc(this.props.winningInfo.passengerId).get().then(passenger => {
-			this.setState({ passenger: passenger.data() });
+		await store.collection("lots").doc(this.props.lotId).get().then(lot => {
+			this.setState({ lot: lot.data() });
+			store.collection("users").doc(lot.data().passengerId).get().then(passenger => {
+				this.setState({ passenger: passenger.data() })
+			})
 		})
 	}
-	
-	_toggleModal = () => this.setState({ isModalVisible: !this.state.isModalVisible });
-	
-	handleGetDirections = () => {
+		
+	// It seems like these functions could be written more concisely / better, but I don't think it's really a big deal, since it doesn't really affect proformance, and they're pretty readable
+	handleDirectionsToStart = () => {
 		const data = {
 			destination: {
 				latitude: this.props.pickupLocation.coords.latitude,
@@ -43,7 +43,7 @@ export default class Winner extends React.Component {
 		getDirections(data);
 	}
 	
-	handleGetDirectionsTwo = () => {
+	handleDirectionsForTrip = () => {
 		const data = {
 			destination: {
 				// latitude: this.props.pickupLocation.coords.latitude,
@@ -67,19 +67,14 @@ export default class Winner extends React.Component {
 	render () {
 		return (
 			<View>
-				<Button title="Show Modal" onPress={this._toggleModal} />
-				<Modal isVisible={this.state.isModalVisible}>
-				<View>
-					<Text> You are the Winner!</Text>
-					<Text>Passenger Name: {this.state.passenger.name}</Text>
-					<Button title={"" + this.state.passenger.phone} onPress={() => { call({ number: this.state.passenger.phone, prompt: true }).catch(console.error) }} />
-					<Text>Passenger location</Text>
-					<Text>Destination time {this.props.winningInfo.pickupTime.seconds}</Text>
-					<Button title="Drive to Passenger!" onPress={this.handleGetDirections} />
-					<Button title="Drive to Passenger's destination!" onPress={this.handleGetDirectionsTwo} />
-					<Button title="Close" onPress={this._toggleModal} />
-				</View>
-				</Modal>
+				<Text> You are the Winner!</Text>
+				<Text>Passenger Name: {this.state.passenger.name}</Text>
+				<Button title={"" + this.state.passenger.phone} onPress={() => { call({ number: this.state.passenger.phone, prompt: true }).catch(console.error) }} />
+				<Text>Passenger location</Text>
+				<Text>Destination time {this.props.winningInfo.pickupTime.seconds}</Text>
+				<Button title="Drive to Passenger!" onPress={this.handleDirectionsToStart} />
+				<Button title="Drive to Passenger's destination!" onPress={this.handleDirectionsForTrip} />
+				<Button title="Close" onPress={() => {}} />
 			</View>
 		);
 	}
